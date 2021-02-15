1f7d7d672935347b8d45d33a119c8484e2ee1969
Yannick Houbrix <sh33dafi@users.noreply.github.com>
2021-02-16 00:17:09 +0100

    feat: updated listen address

--- src/main.js
+++ src/main.js
@@ -50,7 +50,7 @@ fastify.get('/metrics', async () => {
 
 const start = async () => {
     try {
-        await fastify.listen(9092);
+        await fastify.listen(9092, '0.0.0.0');
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
